dab09449ccaa969bf2ce51bdd7458747b22ce160
DNEK <dnek.app@gmail.com>
2024-09-23 20:20:40 +0900

    don't execute autoplay cancel after user performed play operation

--- nicovideo-autoplay-canceler.user.js
+++ nicovideo-autoplay-canceler.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        nicovideo-autoplay-canceler
 // @namespace   https://github.com/dnek
-// @version     2.0
+// @version     2.1
 // @author      dnek
 // @description ニコニコ動画で動画の自動再生が開始された直後に自動的に一時停止します。停止に少しラグがあるため、停止時の再生時間が3秒未満の場合は0秒まで戻します。「nicovideo-next-video-canceler」「nicovideo-player-expander」は別のスクリプトです。
 // @description:ja    ニコニコ動画で動画の自動再生が開始された直後に自動的に一時停止します。停止に少しラグがあるため、停止時の再生時間が3秒未満の場合は0秒まで戻します。「nicovideo-next-video-canceler」「nicovideo-player-expander」は別のスクリプトです。
@@ -19,12 +19,71 @@
     let lastHref = '';
     let isCanceled = false;
 
+    const observer = new MutationObserver((mutationList, observer) => {
+        mutationList.filter(mutation => mutation.type === 'childList').forEach(mutation => {
+            for (const node of mutation.addedNodes) {
+                if (
+                    node.nodeType === 1 &&
+                    node.innerHTML.includes('aria-label="再生する"')
+                ) {
+                    const buttonEl = node.querySelector('button[aria-label="再生する"]');
+                    buttonEl.addEventListener('click', () => {
+                        if (isCanceled) {
+                            return;
+                        }
+
+                        isCanceled = true;
+                        console.log(`Autoplay cancel won't be executed because play button was clicked.`);
+                    });
+                    console.log('nicovideo-autoplay-canceler listener added to button.');
+                }
+            }
+        });
+    });
+    const options = {
+        childList: true,
+        subtree: true,
+    };
+    observer.observe(document.body, options);
+
+    document.addEventListener('keydown', (e) => {
+        if (isCanceled) {
+            return;
+        }
+
+        if (e.target && ['input', 'textarea'].includes(e.target.tagName.toLowerCase())) {
+            return;
+        }
+        if (e.isComposing || e.altKey || e.ctrlKey || e.metaKey || e.shiftKey || e.repeat) {
+            return;
+        }
+
+        if (e.code === 'Space' || e.code === 'KeyK') {
+            isCanceled = true;
+            console.log(`Autoplay cancel won't be executed because ${e.code} was pressed.`);
+        }
+    });
+
     setInterval(() => {
         const href = location.href;
         if (lastHref !== href) {
             lastHref = href;
             console.log(`href changed to ${href}`);
             isCanceled = false;
+        }
+
+        const ctxEl = document.getElementById('menu::r5::ctx-trigger');
+        if (ctxEl !== null && !ctxEl.dataset.napcTriggered) {
+            ctxEl.addEventListener('click', () => {
+                if (isCanceled) {
+                    return;
+                }
+
+                isCanceled = true;
+                console.log(`Autoplay cancel won't be executed because player was clicked.`);
+            });
+            ctxEl.dataset.napcTriggered = true;
+            console.log('nicovideo-autoplay-canceler listener added to ctx.');
         }
 
         if (isCanceled) {
